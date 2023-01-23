9c77534f1d2fcecdcab53b4ea391b89f9d2e8b4b
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-01-23 15:58:32 +0300

    Easy

--- work3/Program.cs
+++ work3/Program.cs
@@ -1,1 +1,10 @@
 ﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите день недели по номеру от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+{
+    if (number < 6)
+        Console.Write(" Нет ");
+    else
+        Console.Write(" Да ");
+    return;
+}
